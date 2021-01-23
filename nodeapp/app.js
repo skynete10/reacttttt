d0e74584d9cdbfacbdd15d4c3f2004f0b82a5c3b
skynete10 <skynete10@gmail.com>
2021-01-23 11:46:00 +0200

    1

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -3,19 +3,35 @@ const { request } = require('http');
 const app = express()
 const port = 3000
 
-const path=require('path');
-const app_router=express.Router;
+let router_add = express.Router();
+let router_list = express.Router();
 
-const mongoadd=require(path.join(__dirname+'/mongoadd'));
-app.get('/mongoadd',(request,response)=>{
-  response.json(mongoadd);
+router_add.get('/', function (req, res) {
+  var MongoClient = require('mongodb').MongoClient;
+  //Create a database named "test" and collection named "users":
+  var url = "mongodb://localhost:27017/test";
+
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("test");
+
+    var myobj = { name: 'req.query.name', age: 'req.query.age' };
+    dbo.collection("users").insertOne(myobj, function (err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
+    db.close();
+  });
 });
 
-const mongolist=require(path.join(__dirname+'/mongolist'));
-app.get('/mongolist',(request,response)=>{
-  response.json(mongolist);
+router_list.get('/', function (req, res) {
+  res.json({ 'asd': 'Ping Successfull' });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.use('/mongoadd', router_add);
+app.use('/mongolist', router_list);
+
+app.listen(port, function () {
+  console.log('Server is running at PORT:', port);
+});
